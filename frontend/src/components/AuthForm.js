ac6bba7729cb6bc50d812c7261cb6d7ba730c9a8
summereuna <summereuna@gmail.com>
2023-06-21 16:34:39 +0900

    Feat signup process

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,28 +1,51 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  //status 422/401이 나올 경우(사용자 인증 문제 발생) 해당 양식 전송한 작업 함수가 리턴한 데이터
+  const data = useActionData();
+
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        <h1>{isLogin ? "로그인" : "회원가입"}</h1>
+        {/* data가 있고 오류가 발생한게 맞다면, Object.values()를 활용해 errors 객체의 모든 값을 살피기 */}
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">이메일</label>
           <input id="email" type="email" name="email" required />
         </p>
         <p>
-          <label htmlFor="image">Password</label>
+          <label htmlFor="image">비밀번호</label>
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
-            {isLogin ? "Create new user" : "Login"}
+            {isLogin ? "회원가입" : "로그인"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "제출 중..." : "제출"}
+          </button>
         </div>
       </Form>
     </>
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,55 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+//AuthForm이 있는 라우트와 동일한 라우트인 AuthenticationPage
+//AuthForm의 Form이 전송될 때 마다 액션 트리거
+export const action = async ({ request }) => {
+  //쿼리 매개변수 확인 위해 브라우저 내장 URL 생성자 함수 사용하여 searchParams 객체에 접근
+  const searchParams = new URL(request.url).searchParams;
+  //mode 잡아오고, 아직 undefined일 경우 기본 값을 login으로 해주고
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "미지원 모드입니다." }, { status: 422 });
+  }
+
+  //전송되 폼 데이터에 접근
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  //응답
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+  //JSON 포맷으로 변환
+
+  //응답 처리 코드
+  //유효하지 않은 자격 증명으로 로그인 시도하여 백엔드에서 오류 받은 경우
+  //authForm에 데이터 리턴하여 오류 메시지 보여주기
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  //응답 오류인 경우
+  if (!response.ok) {
+    throw json({ message: "사용자 인증 불가" }, { status: 500 });
+  }
+
+  //응답 성공시 백엔드에서 얻는 토큰 관리...
+
+  //성공시 사용자 홈으로 리다이렉트
+  return redirect("/");
+};
