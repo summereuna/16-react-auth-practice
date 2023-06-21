1da67558a249ebfbc676acdc8e2bd3da532a103b
summereuna <summereuna@gmail.com>
2023-06-21 17:21:03 +0900

    Set english to korean

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,21 +1,21 @@
-import { useRouteError } from 'react-router-dom';
-import MainNavigation from '../components/MainNavigation';
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
-import PageContent from '../components/PageContent';
+import PageContent from "../components/PageContent";
 
 function ErrorPage() {
   const error = useRouteError();
 
-  let title = 'An error occurred!';
-  let message = 'Something went wrong!';
+  let title = " 에러 발생!";
+  let message = "뭔가 잘못 되었습니다.";
 
   if (error.status === 500) {
     message = error.data.message;
   }
 
   if (error.status === 404) {
-    title = 'Not found!';
-    message = 'Could not find resource or page.';
+    title = "404: Not found!";
+    message = "리소스 또는 페이지를 찾을 수 없습니다.";
   }
 
   return (
